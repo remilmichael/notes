a2efcd3607def04d68fa5d0e40458eba448b4af8
Remil Michael <mail@remil.me>
2020-08-06 09:03:17 +0530

    added jsdoc in message action creator

--- src/store/actions/message.js
+++ src/store/actions/message.js
@@ -1,5 +1,13 @@
 import * as actions from './actionTypes';
 
+/**
+ * Action creator to set global message
+ * 
+ * @function setMessage
+ * @param {String} message - Info messages created
+ * @param {String} type - Type of message, to generate appropriate alert
+ * @returns {Object} - Redux action type and message payload, and its type
+ */
 export const setMessage = (message, type) => {
     return {
         type: actions.SET_GLOBAL_MESSAGE,
@@ -8,6 +16,12 @@ export const setMessage = (message, type) => {
     };
 }
 
+/**
+ * Action creator to clear the existing global message
+ * 
+ * @function unsetMessage
+ * @return {Object} - Redux action type
+ */
 export const unsetMessage = () => {
     return {
         type: actions.UNSET_GLOBAL_MESSAGE,
