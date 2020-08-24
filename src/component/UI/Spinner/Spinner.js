cc82317b425ff2370dc4b16bea8bbe517f382a92
Remil Michael <mail@remil.me>
2020-08-24 10:17:55 +0530

    add data-testid to spinner div tag

--- src/component/UI/Spinner/Spinner.js
+++ src/component/UI/Spinner/Spinner.js
@@ -6,7 +6,7 @@ import './Spinner.css';
 const spinner = () => (
     <Row className="text-center" style={{height: '100vh'}}>
         <Col className="pl-0 col-sm-4 offset-sm-4 align-self-center">
-            <div className="lds-hourglass"></div>
+            <div data-testid="spinner" className="lds-hourglass"></div>
         </Col>
     </Row>
 );
